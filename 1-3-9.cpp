83013e9e68717cabbd5d2eae892c67ff64971066
人在灯火阑珊处 <2772540362@qq.com>
2023-01-03 23:00:13 +0800

    输出内容带双引号和百分号

---
+++ 1-3-9.cpp
@@ -0,0 +1,14 @@
+//在双引号“ ”中再次出现单引号或者\时，需要在其前面再加上一个\才可以输出其本身。
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    cout << "\"Genius is 1% inspiration and 99% perspiration.\"" << endl;
+//    return 0;
+//}
+#include <stdio.h>
+int main() {
+	printf("\"Genius is 1%% inspiration and 99%% perspiration.\"");
+	return 0;
+}
+
