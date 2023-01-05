ff9562da9f661a4b69ddaf46af5891d7e1bfcccd
人在灯火阑珊处 <2772540362@qq.com>
2023-01-05 22:52:44 +0800

    空格不能漏注意字符

---
+++ 1-5-2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << char ('h' + 1);
+    cout << char ('e' + 1);
+    cout << char ('l' + 1);
+    cout << char ('l' + 1);
+    cout << char ('o' + 1);
+    
+    cout << char (' ' + 1);
+    
+    cout << char ('w' + 1);
+    cout << char ('o' + 1);
+    cout << char ('r' + 1);
+    cout << char ('l' + 1);
+    cout << char ('d' + 1);
+    return 0;
+}
+
