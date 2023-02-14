c247376073f4d11801d2034a5cc39bd2d21df07e
人在灯火阑珊处 <2772540362@qq.com>
2023-02-14 21:54:22 +0800

    1

---
+++ 2-13-1.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main (void){
+	const int number = 10;
+	int x;
+	int count[number];
+	int i ;
+	for (i = 0 ;i < number;i++){
+		count[i] = 0;
+	}
+	scanf("%d",&x);
+	while (x != -1){
+		if (x >= 0 && x <= 9){
+			count[x] ++;
+			
+		}
+		scanf("%d",&x);
+	}
+	for (i=0 ; i < number; i++){
+		printf("%d:%d\n",i,count[i]);
+	}
+	return 0;
+} 
