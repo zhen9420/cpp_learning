0307c27ad1c21ed95396f00ac26bcd476f6352b7
人在灯火阑珊处 <2772540362@qq.com>
2023-02-15 22:41:08 +0800

    5

---
+++ 2_15_5_turn2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+using namespace std;
+
+int a[3];
+
+int main()
+{
+    cin>>a[0]>>a[1]>>a[2];
+    sort(a,a+3);
+    cout<<a[0]<<' '<<a[1]<<' '<<a[2];
+    return 0;
+}
