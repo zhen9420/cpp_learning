9146dbbf8f33ebf7f04b5eb8ec28ee793c6c96cc
人在灯火阑珊处 <2772540362@qq.com>
2023-02-15 22:40:15 +0800

    2

---
+++ 2_15_2_apples.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+	int x;
+	scanf("%d",&x);
+	if( x > 1){
+		printf("Today, I ate %d apples.",x);
+	}
+	else {
+		printf("Today, I ate %d apple.",x);
+	}
+	return 0;
+}
