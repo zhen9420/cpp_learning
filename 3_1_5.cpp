c3a4ec7e2b994458169f52f58c1a5176cc8cb143
人在灯火阑珊处 <2772540362@qq.com>
2023-03-08 23:54:47 +0800

    5

---
+++ 3_1_5.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int k;
+	cin>>k;
+	int a=0;
+	int add =0;
+	for(int i = 1;i<=k;i++)
+	{
+		add +=i;
+		if(add>=k)
+		{
+			a = i;
+			break;
+		}
+	}
+ 	for(int j = 0;j<)
+}
