5ca1f3401b9d90429f6ac894c4d52a5cd2ab86d6
人在灯火阑珊处 <2772540362@qq.com>
2023-03-14 12:19:59 +0800

    2

---
+++ 3_10_1.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	int x=1,y=0,a=0,b=0;
+	switch(x)
+	{
+		case 1: switch(y)
+						{
+							case 0:a++;
+							break;
+						}
+						break;
+		case 2: a++;b++;
+		break;
+	}
+	cout<<a<<b;
+	
+ } 
